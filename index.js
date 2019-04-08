83c444558a9028db8941a341764499e9f1ec7609
max-powell <github email address>
2019-04-08 12:04:09 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.slice(0,1).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z1-9-'' ]/gi, '')
+  }
+
+  static titleize (string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const output = string.split(' ').map(function(word) {
+      if (!exceptions.includes(word)) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+    return output[0].toUpperCase() + output.slice(1)
+  }
 }
